35e08ad8c9d4c48bf754c44cf3af18001412d481
yuchen1121 <xie.yuch@husky.neu.edu>
2019-11-03 23:53:41 -0800

    Remove redundance

--- leetcode/problem1057.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-	def rearrangeBarcodes(self, barcodes):
-		
-		
-if __name__ == '__main__':
-	s = Solution()
-	barcodes =[1,1,1,1,2,2,3,3]
-	print s.rearrangeBarcodes(barcodes)
--- leetcode/problem51.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-	def solveNQueen(self, n):
-		
-		return 
-if __name__ == '__main__':
-	n = 8
-	s = Solution()
-	print s.solveNQueen(n)
