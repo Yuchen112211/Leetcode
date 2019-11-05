b4ea7d5f4cdb4bba9a6cc17c92f20de15461d7bc
yuchen1121 <xie.yuch@husky.neu.edu>
2019-11-04 19:08:12 -0800

    Added description.

--- problem1124.py
+++ problem1124.py
@@ -19,7 +19,24 @@ Input: hours = [9,9,6,0,6,6,9]
 Output: 3
 Explanation: The longest well-performing interval is [9,9,6].
 
+Solution:
+This is a dp problem. Initialize two variables, Sum and ans.
 
+Iterate all through the hours(the given list), if current hour is bigger than 8, plus 1 to sum, else minus 1 to sum.
+
+Then we see whether the sum is bigger than 0. If so, which means current day is still in a well-performing interval.
+So we add 1 to ans.
+
+If the sum is less than 0, which means the well-performing interval has come to the end, which means the day with more
+than 8 hours is currently equal or less than the days with no more than 8. If the sum-1 is in the cache, which means there
+is a well-performing ending before. Make the ans to be the biggest of current length and ans.
+
+The Sum - 1 is that where is the start of the last valid well-performing index starts. When current Sum is 1, we need to find 
+whether 0 is in the cache, and compute the longest well-performing interval.
+
+cache.setdefault is similar to dict d[Sum] = i.
+
+The i would give us the information about where the well-performing interval starts.
 '''
 
 
@@ -29,8 +46,9 @@ class Solution:
 		cache = {}
 		for i, n in enumerate(hours):
 			Sum = Sum + 1 if n > 8 else Sum - 1
-			if Sum > 0: ans = i + 1
-			if Sum-1 in cache:
+			if Sum > 0:
+				ans = i + 1
+			if Sum - 1 in cache:
 				ans = max(ans, i - cache[Sum-1])
 			cache.setdefault(Sum, i)
 		return ans
@@ -39,3 +57,4 @@ class Solution:
 if __name__ == '__main__':
 	s = Solution()
 	hourst = [9,9,6,0,6,6,9]
+	print s.longestWPI(hourst)
