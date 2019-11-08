366bac547da2eebf112e859b8e5864e03847f011
yuchen1121 <xie.yuch@husky.neu.edu>
2019-11-08 00:12:08 -0800

    Added description

--- problem200.py
+++ problem200.py
@@ -1,3 +1,40 @@
+'''
+
+200. Number of Islands
+Medium
+
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+Input:
+11110
+11010
+11000
+00000
+
+Output: 1
+
+Example 2:
+
+Input:
+11000
+11000
+00100
+00011
+
+Output: 3
+
+Solution:
+Use a BFS, starting with any cell that contains a 1. Set its neightbors to be 0
+and push them into the stack. In the for loop, everytime we encounter a 1, we plus
+1 to count.
+
+The count outside the loop means the number of the unions.
+
+'''
+
+
 class Solution(object):
 	def numIslands(self, grid):
 		"""
