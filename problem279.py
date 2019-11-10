90cd0dee99061f2770bb7c0055a6d39c0265eea2
yuchen1121 <xie.yuch@husky.neu.edu>
2019-11-10 00:53:52 -0800

    Added description

--- problem279.py
+++ problem279.py
@@ -1,3 +1,30 @@
+'''
+
+279. Perfect Squares
+Medium
+
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+Example 1:
+
+Input: n = 12
+Output: 3 
+Explanation: 12 = 4 + 4 + 4.
+
+Example 2:
+
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+
+Solution:
+Use dp.
+Generating current number by iterating all sqaure number before.
+Current_num is the possible base to fomating the current number.dp[i] = dp[i-k*k] because
+from i-k*k to i we need only a k square to form i.
+Dp solution here is very efficient and easy to understand.
+
+'''
 class Solution(object):
     def numSquares(self, n):
         """
