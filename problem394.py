13fe73d50915bd39977d710ca909df2cf6d9b793
yuchen1121 <xie.yuch@husky.neu.edu>
2019-11-11 21:08:14 -0800

    Added description

--- problem394.py
+++ problem394.py
@@ -1,3 +1,32 @@
+'''
+
+394. Decode String
+Medium
+
+Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+
+Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there won't be input like 3a or 2[4].
+
+Examples:
+
+s = "3[a]2[bc]", return "aaabcbc".
+s = "3[a2[c]]", return "accaccacc".
+s = "2[abc]3[cd]ef", return "abcabccdcdcdef".
+
+Solution:
+This is a good practice of stack manipulation.
+Any character that is not ], push it back into the stack.
+
+When encounter a ] character, until we encounter the [, we pop out everything and record them as the
+string that we need to use. After we popped out the [, we find out the previous number that should be
+used to multiply the string. Simply use isdigit() method to determine the number.
+
+Very interesting question, can be studied several times.
+'''
 class Solution:
     def decodeString(self, s):
         stack = []
--- problem4.py
+++ problem4.py
@@ -1,26 +1,41 @@
-class Solution(object):
-	def findMedianSortedArrays(self, nums1, nums2):
-		index1 = len(nums1)/2
-		index2 = len(nums2)/2
-		is_odd_1 = len(nums1) % 2
-		is_odd_2 = len(nums2) % 2
-		while index1 > 0 and index1 < len(nums1) and index2 > 0 and index2 < len(nums2):
-			smaller1 = nums1[:index1]
-			bigger1 = nums1[index1:]
-			smaller2 = nums2[:index2]
-			bigger2 = nums2[index2:]
-			if is_odd_1:
-				bigger1 = bigger1[1:]
-			if is_odd_2:
-				bigger2 = bigger2[1:]
-			if smaller1[-1] > bigger2[0]:
-				index1 -= 1
-				index2 += 1
-			if smaller2[-1] > bigger1[0]:
-				index1 += 1
-				index2 -= 1
+'''
 
-		print nums1[index1],nums2[index2]
+Watch this video -https://www.youtube.com/watch?v=LPFhl65R7ww&t=1013s
+You will understand it.
+
+'''
+def median(A, B):
+	m, n = len(A), len(B)
+	if m > n:
+		A, B, m, n = B, A, n, m
+	if n == 0:
+		raise ValueError
+
+	imin, imax, half_len = 0, m, (m + n + 1) / 2
+	while imin <= imax:
+		i = (imin + imax) / 2
+		j = half_len - i
+		if i < m and B[j-1] > A[i]:
+			# i is too small, must increase it
+			imin = i + 1
+		elif i > 0 and A[i-1] > B[j]:
+			# i is too big, must decrease it
+			imax = i - 1
+		else:
+			# i is perfect
+
+			if i == 0: max_of_left = B[j-1]
+			elif j == 0: max_of_left = A[i-1]
+			else: max_of_left = max(A[i-1], B[j-1])
+
+			if (m + n) % 2 == 1:
+				return max_of_left
+
+			if i == m: min_of_right = B[j]
+			elif j == n: min_of_right = A[i]
+			else: min_of_right = min(A[i], B[j])
+
+			return (max_of_left + min_of_right) / 2.0
 
 
 if __name__  == '__main__':
